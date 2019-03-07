0bd76200420c5b5d48686960177297d65a151d2e
James <jamesmorts@outlook.com>
2019-03-07 19:27:54 +1000

    1.1
    Features:
    - Major JS rewrite
    - Adding colours now scales FAR better
    - Changed animation speeds

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
+function buttonClicked($element) {
+  copyStringToClipboard($element.target.value);
+}
+
+var classname = document.getElementsByClassName("colourBut");
+for (var i = 0; i < classname.length; i++) {
+  classname[i].style.backgroundColor = classname[i].value;
+  console.log(classname[i].parentNode.innerHTML);
+}
+
+$(function() {
+   $('.colourBut').click(buttonClicked);
+});
+
+function copyStringToClipboard (str) {
+   var el = document.createElement('textarea');
+   el.value = str;
+   el.setAttribute('readonly', '');
+   el.style = {position: 'absolute', left: '-9999px'};
+   document.body.appendChild(el);
+   el.select();
+   document.execCommand('copy');
+   document.body.removeChild(el);
 }
